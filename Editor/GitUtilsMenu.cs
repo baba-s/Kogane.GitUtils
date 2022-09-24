bd7a6e8afe65c6df68290da9a1450d5478b58fcc
baba-s <shotababa1206@gmail.com>
2022-09-24 18:56:39 +0900

    fix

--- Editor/GitUtilsMenu.cs
+++ Editor/GitUtilsMenu.cs
@@ -1,130 +1,131 @@
-﻿using System;
-using UnityEditor;
-using UnityEngine;
+﻿// using System;
+// using UnityEditor;
+// using UnityEngine;
+//
+// namespace Kogane
+// {
+//     /// <summary>
+//     /// Git に関するメニューを管理するクラス
+//     /// </summary>
+//     public static class GitUtilsMenu
+//     {
+//         //================================================================================
+//         // 定数
+//         //================================================================================
+//         public const string ITEM_NAME_ROOT = "Kogane/git/"; // Unity メニューのルート
+//
+//         //================================================================================
+//         // イベント（static）
+//         //================================================================================
+//         /// <summary>
+//         /// <para>Unity メニューからスクリプトを生成する時に呼び出される関数</para>
+//         /// <para>Unity メニューからスクリプトを生成する処理を外部から登録できます</para>
+//         /// </summary>
+//         public static event Action OnForceGenerate;
+//
+//         //================================================================================
+//         // 関数（static）
+//         //================================================================================
+//         /// <summary>
+//         /// Unity メニューからスクリプトを生成するための関数
+//         /// </summary>
+//         [MenuItem( ITEM_NAME_ROOT + "Git の情報を管理するスクリプトを生成" )]
+//         private static void ForceGenerate()
+//         {
+//             if ( OnForceGenerate == null )
+//             {
+//                 Debug.LogWarning( "Git の情報を管理するスクリプトを生成する処理が登録されていません" );
+//                 return;
+//             }
+//
+//             OnForceGenerate();
+//         }
+//
+//         /// <summary>
+//         /// Unity メニューからブランチ名をログ出力して確認するための関数
+//         /// </summary>
+//         [MenuItem( ITEM_NAME_ROOT + "ブランチ名/ログ出力" )]
+//         private static void LogBranchName()
+//         {
+//             Debug.Log( $"{GitUtils.LoadBranchName()}" );
+//         }
+//
+//         /// <summary>
+//         /// Unity メニューからブランチ名をクリップボードにコピーするための関数
+//         /// </summary>
+//         [MenuItem( ITEM_NAME_ROOT + "ブランチ名/クリップボードにコピー" )]
+//         private static void CopyBranchName()
+//         {
+//             GUIUtility.systemCopyBuffer = GitUtils.LoadBranchName();
+//         }
+//
+//         /// <summary>
+//         /// Unity メニューからコミットハッシュをログ出力して確認するための関数
+//         /// </summary>
+//         [MenuItem( ITEM_NAME_ROOT + "コミットハッシュ/ログ出力" )]
+//         private static void LogCommitHash()
+//         {
+//             Debug.Log( $"{GitUtils.LoadCommitHash()}" );
+//         }
+//
+//         /// <summary>
+//         /// Unity メニューからコミットハッシュをクリップボードにコピーして確認するための関数
+//         /// </summary>
+//         [MenuItem( ITEM_NAME_ROOT + "コミットハッシュ/クリップボードにコピー" )]
+//         private static void CopyCommitHash()
+//         {
+//             GUIUtility.systemCopyBuffer = GitUtils.LoadCommitHash();
+//         }
+//
+//         /// <summary>
+//         /// Unity メニューからコミットハッシュ（短縮版）をログ出力して確認するための関数
+//         /// </summary>
+//         [MenuItem( ITEM_NAME_ROOT + "コミットハッシュ（短縮版）/ログ出力" )]
+//         private static void LogShortCommitHash()
+//         {
+//             Debug.Log( $"{GitUtils.LoadShortCommitHash()}" );
+//         }
+//
+//         /// <summary>
+//         /// Unity メニューからコミットハッシュ（短縮版）をクリップボードにコピーして確認するための関数
+//         /// </summary>
+//         [MenuItem( ITEM_NAME_ROOT + "コミットハッシュ（短縮版）/クリップボードにコピー" )]
+//         private static void CopyShortCommitHash()
+//         {
+//             GUIUtility.systemCopyBuffer = GitUtils.LoadShortCommitHash();
+//         }
+//
+//         /// <summary>
+//         /// Unity メニューからコミットログをログ出力して確認するための関数
+//         /// </summary>
+//         [MenuItem( ITEM_NAME_ROOT + "コミットログ/ログ出力" )]
+//         private static void LogCommitLog()
+//         {
+//             var option = new CommitLogOption
+//             (
+//                 count: 10,
+//                 isNoMerges: false,
+//                 format: "%h %cd %cn %s"
+//             );
+//
+//             Debug.Log( $"{GitUtils.LoadCommitLog( option )}" );
+//         }
+//
+//         /// <summary>
+//         /// Unity メニューからコミットログをクリップボードにコピーして確認するための関数
+//         /// </summary>
+//         [MenuItem( ITEM_NAME_ROOT + "コミットログ/クリップボードにコピー" )]
+//         private static void CopyCommitLog()
+//         {
+//             var option = new CommitLogOption
+//             (
+//                 count: 10,
+//                 isNoMerges: false,
+//                 format: "%h %cd %cn %s"
+//             );
+//
+//             GUIUtility.systemCopyBuffer = GitUtils.LoadCommitLog( option );
+//         }
+//     }
+// }
 
-namespace Kogane
-{
-    /// <summary>
-    /// Git に関するメニューを管理するクラス
-    /// </summary>
-    public static class GitUtilsMenu
-    {
-        //================================================================================
-        // 定数
-        //================================================================================
-        public const string ITEM_NAME_ROOT = "Kogane/git/"; // Unity メニューのルート
-
-        //================================================================================
-        // イベント（static）
-        //================================================================================
-        /// <summary>
-        /// <para>Unity メニューからスクリプトを生成する時に呼び出される関数</para>
-        /// <para>Unity メニューからスクリプトを生成する処理を外部から登録できます</para>
-        /// </summary>
-        public static event Action OnForceGenerate;
-
-        //================================================================================
-        // 関数（static）
-        //================================================================================
-        /// <summary>
-        /// Unity メニューからスクリプトを生成するための関数
-        /// </summary>
-        [MenuItem( ITEM_NAME_ROOT + "Git の情報を管理するスクリプトを生成" )]
-        private static void ForceGenerate()
-        {
-            if ( OnForceGenerate == null )
-            {
-                Debug.LogWarning( "Git の情報を管理するスクリプトを生成する処理が登録されていません" );
-                return;
-            }
-
-            OnForceGenerate();
-        }
-
-        /// <summary>
-        /// Unity メニューからブランチ名をログ出力して確認するための関数
-        /// </summary>
-        [MenuItem( ITEM_NAME_ROOT + "ブランチ名/ログ出力" )]
-        private static void LogBranchName()
-        {
-            Debug.Log( $"{GitUtils.LoadBranchName()}" );
-        }
-
-        /// <summary>
-        /// Unity メニューからブランチ名をクリップボードにコピーするための関数
-        /// </summary>
-        [MenuItem( ITEM_NAME_ROOT + "ブランチ名/クリップボードにコピー" )]
-        private static void CopyBranchName()
-        {
-            GUIUtility.systemCopyBuffer = GitUtils.LoadBranchName();
-        }
-
-        /// <summary>
-        /// Unity メニューからコミットハッシュをログ出力して確認するための関数
-        /// </summary>
-        [MenuItem( ITEM_NAME_ROOT + "コミットハッシュ/ログ出力" )]
-        private static void LogCommitHash()
-        {
-            Debug.Log( $"{GitUtils.LoadCommitHash()}" );
-        }
-
-        /// <summary>
-        /// Unity メニューからコミットハッシュをクリップボードにコピーして確認するための関数
-        /// </summary>
-        [MenuItem( ITEM_NAME_ROOT + "コミットハッシュ/クリップボードにコピー" )]
-        private static void CopyCommitHash()
-        {
-            GUIUtility.systemCopyBuffer = GitUtils.LoadCommitHash();
-        }
-
-        /// <summary>
-        /// Unity メニューからコミットハッシュ（短縮版）をログ出力して確認するための関数
-        /// </summary>
-        [MenuItem( ITEM_NAME_ROOT + "コミットハッシュ（短縮版）/ログ出力" )]
-        private static void LogShortCommitHash()
-        {
-            Debug.Log( $"{GitUtils.LoadShortCommitHash()}" );
-        }
-
-        /// <summary>
-        /// Unity メニューからコミットハッシュ（短縮版）をクリップボードにコピーして確認するための関数
-        /// </summary>
-        [MenuItem( ITEM_NAME_ROOT + "コミットハッシュ（短縮版）/クリップボードにコピー" )]
-        private static void CopyShortCommitHash()
-        {
-            GUIUtility.systemCopyBuffer = GitUtils.LoadShortCommitHash();
-        }
-
-        /// <summary>
-        /// Unity メニューからコミットログをログ出力して確認するための関数
-        /// </summary>
-        [MenuItem( ITEM_NAME_ROOT + "コミットログ/ログ出力" )]
-        private static void LogCommitLog()
-        {
-            var option = new CommitLogOption
-            (
-                count: 10,
-                isNoMerges: false,
-                format: "%h %cd %cn %s"
-            );
-
-            Debug.Log( $"{GitUtils.LoadCommitLog( option )}" );
-        }
-
-        /// <summary>
-        /// Unity メニューからコミットログをクリップボードにコピーして確認するための関数
-        /// </summary>
-        [MenuItem( ITEM_NAME_ROOT + "コミットログ/クリップボードにコピー" )]
-        private static void CopyCommitLog()
-        {
-            var option = new CommitLogOption
-            (
-                count: 10,
-                isNoMerges: false,
-                format: "%h %cd %cn %s"
-            );
-
-            GUIUtility.systemCopyBuffer = GitUtils.LoadCommitLog( option );
-        }
-    }
-}
